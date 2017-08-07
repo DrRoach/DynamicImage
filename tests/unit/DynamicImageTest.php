e1cd3576b0438f5b29820824b4593a9325b7f258
Ryan Deas <ryandeas1@gmail.com>
2017-08-07 23:20:11 +0100

    Finished initial tests

--- tests/unit/DynamicImageTest.php
+++ tests/unit/DynamicImageTest.php
@@ -26,12 +26,17 @@ class DynamicImageTest extends \Codeception\Test\Unit
     {
     }
 
-    // tests
+    /**
+     *  @group main
+     */
     public function testFull()
     {
         $di = new DynamicImage($this->testData);
     }
 
+    /**
+     *  @group main
+     */
     public function testMissingParams()
     {
         // Make sure by the end of the test error has been set to true
@@ -50,6 +55,9 @@ class DynamicImageTest extends \Codeception\Test\Unit
         $this->assertTrue($error);
     }
 
+    /**
+     *  @group main
+     */
     public function testImageMissing()
     {
         // Make sure by the end of the test error has been set to true
@@ -67,12 +75,75 @@ class DynamicImageTest extends \Codeception\Test\Unit
         $this->assertTrue($error);
     }
 
+    /**
+     *  @group main
+     */
     public function testMissingImageReturn()
     {
         $data = $this->testData;
         $data['image_missing'] = 'triangle.jpg';
         $data['filename'] = 'missing.jpg';
         $di = new DynamicImage($data);
-        $this->assertEquals('triangle-500x500.jpg', $di->file);
+        $this->assertEquals('cache/triangle-500x500.jpg', $di->file);
+    }
+
+    /**
+     *  @group main
+     */
+    public function testUnsupportedExtension()
+    {
+        // Make sure by the end of the test error has been set to true
+        $error = false;
+
+        $data = $this->testData;
+        $data['filename'] = 'surf.gif';
+
+        try {
+            $di = new DynamicImage($data);
+        } catch(Exception $e) {
+            $error = true;
+            $this->assertEquals('The filetype that you gave isn\'t supported.', $e->getMessage());
+        }
+
+        $this->assertTrue($error);
+    }
+
+    /**
+     *  @group cache
+     */
+    public function testCreatingCache()
+    {
+        // Make sure by the end of the test error has been set to true
+        $error = false;
+
+        // Directory of the cache
+        $cacheDir = __dir__ . '/../../cache';
+
+        // If the cache directory exists then delete it
+        if (is_dir($cacheDir)) {
+            // Delete all image file that are in the cache directory
+            foreach (glob($cacheDir . "/*") as $file) {
+                if (is_file($file)) {
+                    unlink($file);
+                }
+            }
+
+            // Delete the cache folder
+            rmdir($cacheDir);
+        }
+        
+        $data = $this->testData;
+
+        try {
+            $di = new DynamicImage($data);
+        } catch (Exception $e) {
+            $error = true;
+            $this->assertEquals('There was a problem when creating the `cache` directory. Please create it manually.', $e->getMessage());
+
+            $output = new \Codeception\Lib\Console\Output([]);
+            $output->writeln(" The cache directory couldn't be created. Please create it manually.");
+        }
+
+        $this->assertTrue($error);
     }
 }
