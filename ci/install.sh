6e9a5f13f7588cfbfd2c1ceda46f566d11619975
DrRoach <ryandeas1@gmail.com>
2018-06-16 15:12:51 +0100

    Added CI

---
+++ ci/install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# We need to install dependencies only for Docker
+[[ ! -e /.dockerenv ]] && exit 0
+
+set -xe
+
+# Install git (the php image doesn't have it) which is required by composer
+apt-get update -yqq
+apt-get install git -yqq
+apt-get install libpng-dev -yqq
+apt-get install libjpeg-dev -yqq
+
+docker-php-ext-configure gd --with-jpeg-dir=/usr/include/
+docker-php-ext-install zip gd
+
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('SHA384', 'composer-setup.php') === '544e09ee996cdf60ece3804abc52599c22b1f40f4323403c44d44fdfdd586475ca9813a858088ffbc1f233e9b180f061') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+php composer.phar install
