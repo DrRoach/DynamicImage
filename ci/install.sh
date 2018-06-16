97c32aeb05a72c3529f7b29ea4d9af0e9e36285f
DrRoach <ryandeas1@gmail.com>
2018-06-16 15:29:48 +0100

    Attempt imagick fix

--- ci/install.sh
+++ ci/install.sh
@@ -11,10 +11,10 @@ apt-get install git -yqq
 apt-get install libpng-dev -yqq
 apt-get install libjpeg-dev -yqq
 apt-get install exif -yqq
-apt-get install php-imagick -yqq
+apt-get install libmagickwand-dev -yqq
 
 docker-php-ext-configure gd --with-jpeg-dir=/usr/include/
-docker-php-ext-install zip gd
+docker-php-ext-install zip gd imagick
 
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php -r "if (hash_file('SHA384', 'composer-setup.php') === '544e09ee996cdf60ece3804abc52599c22b1f40f4323403c44d44fdfdd586475ca9813a858088ffbc1f233e9b180f061') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
